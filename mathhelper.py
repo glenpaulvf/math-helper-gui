504869cec8cc96762da4b86cc78adbc9d9539d7e
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-11-05 21:33:42 -0800

    Define MathHelper class to connect inputBox to outputLabel

--- mathhelper.py
+++ mathhelper.py
@@ -8,8 +8,17 @@ class MathHelper(QMainWindow, Ui_MathHelperWindow):
     def __init__(self):
         super(MathHelper, self).__init__()
         self.setupUi(self)
+        self.__exec()
+        
+    def __exec(self):
+        self.inputBox.returnPressed.connect(self.output)
     
-    
+    def output(self):
+        input = self.inputBox.text()
+        self.outputLabel.setText(input)
+        
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mh = MathHelper()
