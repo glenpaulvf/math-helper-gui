e537e32c91e039026c2d35bddd99677d1358a7cf
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-11-06 01:25:32 -0800

    Allow multiple keywords for SymPy derivative function

--- mathhelper.py
+++ mathhelper.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from sympy import sympify
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
@@ -18,8 +19,55 @@ class MathHelper(QMainWindow, Ui_MathHelperWindow):
     def __parse_input(self):
         input = self.inputBox.text()
         
-        self.__output(str(sympify(input)))
-    
+        derivative_keywords = ["diff", "differentiate", "derivative"]
+        
+        regex_comparam = "^([A-Za-z]+)?(.*)$"
+        regex_expvar = "^\((|.+),\s*(|\w+)\)$"
+        
+        try:
+            # Basic syntax of SymPy:
+            # <command>(<expression>, <variable>)
+            
+            # Extract command
+            com = re.sub(regex_comparam, r"\1", input)
+            
+            # Extract parameters (<expression>, <variable>)
+            param = re.sub(regex_comparam, r"\2", input)
+            
+            # Extract expression
+            exp = re.sub(regex_expvar, r"\1", param)
+            
+            # Extract variable
+            var = re.sub(regex_expvar, r"\2", param)
+            
+            # Allow multiple keywords for derivative function diff
+            for d in derivative_keywords:
+                if com == d:
+                    com = "diff"
+                    break
+            
+            # Concatenate input back together
+            input = com + param 
+            
+            # Output result of SymPy input
+            self.__output(str(sympify(input)))
+        except:
+            try:
+                # Basic SymPy syntax:
+                # <expression>
+                
+                # Extract expression
+                input = re.sub(regex_comparam, r"\2", input)
+                
+                # Output result of SymPy input
+                self.__output(self.__eval(input))
+            except:
+                # No valid input detected
+                
+                # Output error message
+                error_msg = "Error: invalid input"
+                self.__output(error_msg)
+            
     def __output(self, result):
         self.outputLabel.setText(result)
         
